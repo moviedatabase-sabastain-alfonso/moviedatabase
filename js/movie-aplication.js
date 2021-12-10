332ff5450176c378582f49ddc8b7bfb21fdaaac1
Alfonso Carrillo <alfonso.ivan.carrillo@gmail.com>
2021-12-10 12:32:18 -0600

    created functions to retrieve all data, movie by id, edit, delete and create movie

--- js/movie-aplication.js
+++ js/movie-aplication.js
@@ -1,6 +1,60 @@
 "use strict";
 
-$(document).ready((function(){
+(function(){
+
+    const API_URL = `https://upbeat-hail-splash.glitch.me/movies`;
+
+    // function to retrieve/fetch movie info from database
+    let getMovieInfo = () =>{
+        return fetch(API_URL).then(response => response.json()).catch(error => console.error(error));
+    }
+    // calls function
+    getMovieInfo().then(data => console.log(data));
+
+
+    // function to retrieve movie by id
+    let getMovieById =(id) => {
+        return fetch(`${API_URL}/${id}`).then(response => response.json()).catch(error => console.error(error));
+    }
+    // calls function
+    getMovieById(2).then(data => console.log(data));
+
+    // create movie function
+    let createMovie = (movie) =>{
+        let options = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify((movie))
+        }
+        return fetch(API_URL, options).then(response => response.json()).catch(error => console.error(error));
+    }
+
+
+    // start: *** function to delete movie ***
+    let deleteMovie = (id) => {
+        let options ={
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }
+        return fetch(`${API_URL}/${id}`, options).then(response => response.json()).catch(error => console.error(error));
+    }
+    // end: *** delete function ***
+
+    // function to edit a movie
+    let editMovie = (movie) => {
+        let options = {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(movie)
+        }
+        return fetch(`${API_URL}/${movie.id}`, options).then(response => response.json()).catch(error => console.error(error));
+    }
 
 
 
@@ -30,5 +84,4 @@ $(document).ready((function(){
 
 
 
-
-})());
+})();
